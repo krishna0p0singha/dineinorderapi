0dd0f7fe7760255f0ef0db7743c4d354003702e9
cyber0bravo <cyber0bravo@gmail.com>
2025-02-19 16:02:02 +0530

    Project Initiated

---
+++ src/main/java/com/exmaple/dio/DineinorderapiApplication.java
@@ -0,0 +1,13 @@
+package com.exmaple.dio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DineinorderapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DineinorderapiApplication.class, args);
+	}
+
+}
