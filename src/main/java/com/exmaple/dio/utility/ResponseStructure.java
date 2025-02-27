45222ac235485b2ef78ae1b7172d5676447a0375
cyber0bravo <cyber0bravo@gmail.com>
2025-02-27 14:55:55 +0530

    ResponseStructure class Updating general from to Fctory Method and Builder Pattern

---
+++ src/main/java/com/exmaple/dio/utility/ResponseStructure.java
@@ -0,0 +1,22 @@
+package com.exmaple.dio.utility;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+public class ResponseStructure <T>{
+    private int status;
+    private String message;
+    private T data;
+
+    public static <T> ResponseStructure<T> create(HttpStatus status, String message, T data){
+        ResponseStructure<T> responseStructure= new ResponseStructure<>();
+        responseStructure.status= status.value();
+        responseStructure.message=message;
+        responseStructure.data=data;
+        return responseStructure;
+
+    }
+
+}
